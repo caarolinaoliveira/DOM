c90f3c22eca7fe2a8c064cce624382986f3c33b3
caarolinaoliveira <carolina.dsoliveira@icloud.com>
2024-04-23 12:32:06 -0300

    'innerHTML'

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
-
+const titulo = document.querySelector('.app__title')
 
 /*  essa função acima é um evento simples que diz:
 quando clicar no botão 'descanso curto' o html será modificado pelo
@@ -35,6 +35,22 @@ curtoBt.addEventListener('click', () => {
 function alterarContexto(contexto){
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch(contexto) {
+        case "foco": 
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada?<br>
+            <strong class="app__title-strong">Faça uma pausa curta.</strong>`
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar à superfície.<br>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+        default:
+            break;
+
+    }
 }
 
 
